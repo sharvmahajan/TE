4be38a45360bb714611694f4323817df80f13ef9
Sharv Mahajan <sharv.mahajan@gmail.com>
2026-04-07 10:31:53 +0530

    feat: Implement EbookShop servlet with JDBC to display database records

---
+++ A5/EbookShopServlet/src/EbookServlet.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class EbookServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><body>");
+        out.println("<h2>Ebook Shop Details</h2>");
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/ebookshop",
+                    "root",
+                    "");
+
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM ebookshop");
+
+            out.println("<table border=1>");
+            out.println("<tr>");
+            out.println("<th>ID</th>");
+            out.println("<th>Title</th>");
+            out.println("<th>Author</th>");
+            out.println("<th>Price</th>");
+            out.println("<th>Quantity</th>");
+            out.println("</tr>");
+
+            while (rs.next()) {
+
+                out.println("<tr>");
+                out.println("<td>" + rs.getInt("book_id") + "</td>");
+                out.println("<td>" + rs.getString("book_title") + "</td>");
+                out.println("<td>" + rs.getString("book_author") + "</td>");
+                out.println("<td>" + rs.getDouble("book_price") + "</td>");
+                out.println("<td>" + rs.getInt("quantity") + "</td>");
+                out.println("</tr>");
+            }
+
+            out.println("</table>");
+
+            con.close();
+
+        } catch (Exception e) {
+            out.println(e);
+        }
+
+        out.println("</body></html>");
+    }
+}
